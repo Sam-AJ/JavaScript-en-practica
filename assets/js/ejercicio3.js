6dab8532f41073ccd6d6aee38c0297d501e5873f
Samuel Jara <spjara.aravena@gmail.com>
2022-01-29 15:16:04 -0300

    Version Final

--- assets/js/ejercicio3.js
+++ assets/js/ejercicio3.js
@@ -2,7 +2,7 @@ let btnSumar = document.getElementById("btn-sumar");
 let btnRestar = document.getElementById("btn-restar");
 let valor1 = document.getElementById("valor1");
 let valor2 = document.getElementById("valor2");
-
+let resultado = document.querySelector(".resultado");
 
 btnSumar.addEventListener("click", function(){
     var num1 = valor1.value;
@@ -10,7 +10,7 @@ btnSumar.addEventListener("click", function(){
     var num2 = valor2.value;
     num2 = parseInt(num2);
     let suma = num1 + num2;
-    document.querySelector(".resultado").innerHTML = ` ${suma}`;
+    resultado.innerHTML = ` ${suma}`;
 });
 
 btnRestar.addEventListener("click", function(){
@@ -19,9 +19,10 @@ btnRestar.addEventListener("click", function(){
     var num2 = valor2.value;
     num2 = parseInt(num2);
     let resta = num1 - num2;
+
     if (resta < 0) {
-        document.querySelector(".resultado").innerHTML = "0";
+        resultado.innerHTML = "0";
     }else {
-        document.querySelector(".resultado").innerHTML = ` ${resta}`;
+        resultado.innerHTML = ` ${resta}`;
     }
 });
